5c50cb0228e85cbded9170e5968c03c1bf6cd261
Ben Luo <benluo@gmail.com>
2020-08-24 16:24:42 +0800

    Modified android app to more morden component.

--- android/app/src/main/java/biz/benluo/android/makesure_zihua/ui/about/AboutFragment.kt
+++ android/app/src/main/java/biz/benluo/android/makesure_zihua/ui/about/AboutFragment.kt
@@ -4,28 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import biz.benluo.android.makesure_zihua.R
 
 class AboutFragment : Fragment() {
-
-    private lateinit var aboutViewModel: AboutViewModel
-
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        aboutViewModel =
-                ViewModelProviders.of(this).get(AboutViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_about, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        aboutViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
